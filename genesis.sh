e6c7300975ab60a995993c3af6b2e21a54ef627a
Ola Ajibode <ola.ajibode@gmail.com>
2015-12-25 23:45:40 +0000

    start scripting

---
+++ genesis.sh
@@ -0,0 +1,17 @@
+#! /bin/bash 
+
+# Linux Flavour Information
+# echo $(lsb_release -a)
+echo "You are using $( basename $0)"
+echo $BASH_VERSION
+
+# Verbose Shell script running
+# bash -x ./genesis.sh
+
+
+
+# anything other than 0 is an error
+exit 0
+
+
+
