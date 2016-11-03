cb4af685b25dc74b8a8c591d6b8592c5429e6fe4
Ola Ajibode <ola.ajibode@gmail.com>
2016-11-03 19:46:35 +0000

    Service level range added with SLA message for 0%

--- monitor_servers.sh
+++ monitor_servers.sh
@@ -1,32 +1,42 @@
 #! /bin/bash
 
-#  author:  Ola Ajibode
-
 # Monitor servers availability using a cron job
-#  Run script: ./monitor_servers.sh "ohprice.com"
 
 seed_domain=$1
 
 HOST=$seed_domain
 check_count=0
-expected_count=10
+expected_count=4
 
 while [[ $check_count -lt $expected_count ]]; do
 	echo $check_count
 	ping -c 1 $HOST || echo "$HOST unreachable."
 	check_count=$[$check_count+1]
-	sleep 300 
+	sleep 2
 done
 
 echo $check_count
-status_level=($check_count/$expected_count*100)
+status_level=$[$check_count/$expected_count*100]
 echo $status_level
 if [[ $status_level -eq 100 ]]; then
 	#statements
-	echo "Excellent.  $HOST is up 100%"
+	echo "Service Level: EXCELLENT.  $HOST STATUS: $status_level%"
 elif [[ $status_level -lt 80 ]]; then
 	#statements
-	echo "Service is OK.  About 80% availability"
+	echo "Service Level: OK.  $HOST STATUS: $status_level%"
+elif [[ $status_level -lt 60 ]]; then
+	#statements
+	echo "Service Level: AVERAGE.  $HOST STATUS: $status_level%"
+elif [[ $satus_level -lt 45 ]]; then
+	#statements
+	echo "Service Level: DEGRADED. $HOST STATUS: $status_level%"
+else
+	echo "ERROR: Not sure server is running. Check domain. "
 
+fi
+
+if [[ $status_level -eq 100 ]]; then
+	#statements
+	echo "ALERT! 0% Availability! Service is definitely DOWN.  Activate SLA Agreement. Call 0-NUMBER-OLA!"
 fi
 
