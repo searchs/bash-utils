c2ceb92472583db29f426297686948b7129d9e34
Ola Ajibode <ola.ajibode@gmail.com>
2016-11-03 20:03:07 +0000

    Removed echo checking status of various bits.

--- monitor_servers.sh
+++ monitor_servers.sh
@@ -2,30 +2,25 @@
 
 # Monitor servers availability using a cron job
 
-seed_domain=$1
-
-HOST=$seed_domain
+HOST=$1
 check_count=0
 expected_count=4
 
 while [[ $check_count -lt $expected_count ]]; do
-	echo $check_count
 	ping -c 1 $HOST || echo "$HOST unreachable."
 	check_count=$[$check_count+1]
 	sleep 2
 done
 
-echo $check_count
 status_level=$[$check_count/$expected_count*100]
-echo $status_level
 if [[ $status_level -eq 100 ]]; then
-	echo "Service Level: EXCELLENT.  $HOST STATUS: $status_level%"
+	echo "INFO: Service Level: EXCELLENT.  $HOST STATUS: $status_level%"
 elif [[ $status_level -lt 80 ]]; then
-	echo "Service Level: OK.  $HOST STATUS: $status_level%"
+	echo "INFO: Service Level: OK.  $HOST STATUS: $status_level%"
 elif [[ $status_level -lt 60 ]]; then
-	echo "Service Level: AVERAGE.  $HOST STATUS: $status_level%"
+	echo "INFO: Service Level: AVERAGE.  $HOST STATUS: $status_level%"
 elif [[ $satus_level -lt 45 ]]; then
-	echo "Service Level: DEGRADED. $HOST STATUS: $status_level%"
+	echo "WARN: Service Level: DEGRADED. $HOST STATUS: $status_level%"
 else
 	echo "ERROR: Not sure server is running. Check domain. "
 
