096b80486536b5924944a35d3e9fc92d353eacab
Ola Ajibode <ola.ajibode@gmail.com>
2016-11-04 09:14:16 +0000

    Add option to echo statements for nextline

--- monitor_servers.sh
+++ monitor_servers.sh
@@ -1,35 +1,35 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 # Monitor servers availability using a cron job
 
 HOST=$1
 check_count=0
-expected_count=4
+expected_count=2
 
 while [[ $check_count -lt $expected_count ]]; do
-	ping -c 1 $HOST || echo "$HOST unreachable."
+	ping -q -c 1 $HOST || echo -e "$HOST unreachable! "
 	check_count=$[$check_count+1]
-	sleep 2
+	sleep 3
 done
 
 status_level=$[$check_count/$expected_count*100]
 if [[ $status_level -eq 100 ]]; then
-	echo "INFO: Service Level: EXCELLENT.  $HOST STATUS: $status_level%"
+	echo 
+	echo -e "INFO: Service Level: EXCELLENT.  $HOST STATUS: $status_level%"
 elif [[ $status_level -lt 80 ]]; then
-	echo "INFO: Service Level: OK.  $HOST STATUS: $status_level%"
+	echo -e "INFO: Service Level: OK.  $HOST STATUS: $status_level%"
 elif [[ $status_level -lt 60 ]]; then
-	echo "INFO: Service Level: AVERAGE.  $HOST STATUS: $status_level%"
+	echo -e "INFO: Service Level: AVERAGE.  $HOST STATUS: $status_level% "
 elif [[ $satus_level -lt 45 ]]; then
-	echo "WARN: Service Level: DEGRADED. $HOST STATUS: $status_level%"
+	echo -e "WARN: Service Level: DEGRADED. $HOST STATUS: $status_level% "
 else
-	echo "ERROR: Not sure server is running. Check domain. "
+	echo -e "ERROR: Not sure server is running. Check domain. "
 
 fi
 
 # Test
-if [[ $status_level -eq 100 ]]; then
-	echo "ALERT! 0% Availability! Service is definitely DOWN.  Activate SLA Agreement. Call 0-NUMBER-OLA!"
-	mail -s "$HOSt is down now!" email@domain.com <<< "ALERT! $HOST is down. "
+if [[ $status_level -lt 20 ]]; then
+	echo -e "ALERT! 0% Availability! Service is definitely DOWN.  Activate SLA Agreement. Call 0-NUMBER-OLA!"
+	# mail -s "$HOSt is down now!" email@domain.com <<< "ALERT! $HOST is down. "  #SMTP setup needed on linux server
 
 fi
-
