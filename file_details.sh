2164095fb3ec22e1eb9a36e8f733e872bbd94c82
Ola Ajibode <searchs@users.noreply.github.com>
2017-11-13 19:05:48 +0000

    add file hash only to manifest

--- file_details.sh
+++ file_details.sh
@@ -13,10 +13,17 @@ echo $current_time
 
 row_count=`wc -l $filename | awk '{print $1}'`
 file_size=`wc -c $filename | awk '{print $1}'`
-file_sized=`du -h "$filename" | awk '{ print $1}'`
+md5_hash=`md5 $filename`
+# echo $md5_hash
+read -a arr <<< $md5_hash
+# echo $arr
+arr_length=$(echo ${#arr[@]})
+echo $arr_length
+hash_value=$(echo ${arr[3]})
+
 name=$(echo $filename | cut -f 1 -d '.')
-manifest_headers="current_time,file_name,total_rows,file_size_bytes,file_size_more"
+manifest_headers="current_time,file_name,total_rows,file_size_bytes,hash_value"
 manifest_report=manifest_${name}.csv
 # echo $manifest_report
 echo $manifest_headers > $manifest_report
-echo "$current_time,$filename,$row_count,$file_size,$file_sized" >> $manifest_report
+echo "$current_time,$filename,$row_count,$file_size,$hash_value" >> $manifest_report
