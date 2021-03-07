9d5d5158afe1c71c1926b1f322ca9297d8cad454
Ola Ajibode <ola.ajibode@gmail.com>
2021-03-07 13:51:25 +0000

    Add chmod and run command

--- centos_tune.sh
+++ centos_tune.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+##############################################
+
+# chmod +x centos_tune.sh
+# ./centos_tune.sh
+
+##############################################
+
 sudo yum clean all
 sudo yum update -y
 sudo yum upgrade -y
@@ -7,4 +14,4 @@ echo "INFO: Checking for large files that can be removed..."
 sudo find / -type f -size +300M -exec ls -lh {} \;
 
 echo "ADMIN: You can now cleanup the files that you don't want."
-echo ${date}
+echo $(date)
