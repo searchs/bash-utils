ec2eeb0886560bdf707f4452cbdc7754b951ba5d
Ola Ajibode <ola.ajibode@gmail.com>
2021-04-27 08:14:54 +0100

    Fix:  Echo with option for line breaks

--- centos_tune.sh
+++ centos_tune.sh
@@ -3,12 +3,12 @@
 sudo yum clean all
 sudo yum update -y
 sudo yum upgrade -y
-echo "\INFO:  Cleaning old kernels and unnecessary packages on system...\n"
+echo -e "\INFO:  Cleaning old kernels and unnecessary packages on system...\n"
 
 sudo package-cleanup -q --leaves | xargs -r -l1 yum -y remove
 sudo package-cleanup --oldkernel --count=2
 
-echo "\nINFO: Checking for large files that can be removed...\n"
+echo -e "\nINFO: Checking for large files that can be removed...\n"
 sudo find / -type f -size +300M -exec ls -lh {} \;
 
 echo "ADMIN: You can now cleanup the files that you don't want."
