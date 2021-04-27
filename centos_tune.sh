64ee81db9d8fc1f1244e2d7eb289fc97caf46928
Ola Ajibode <ola.ajibode@gmail.com>
2021-04-27 08:12:34 +0100

    Feat:  Add kernel and old package cleanup

--- centos_tune.sh
+++ centos_tune.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
 
-##############################################
-
-# chmod +x centos_tune.sh
-# ./centos_tune.sh
-
-##############################################
-
 sudo yum clean all
 sudo yum update -y
 sudo yum upgrade -y
-echo "INFO: Checking for large files that can be removed..."
+echo "\INFO:  Cleaning old kernels and unnecessary packages on system...\n"
+
+sudo package-cleanup -q --leaves | xargs -r -l1 yum -y remove
+sudo package-cleanup --oldkernel --count=2
+
+echo "\nINFO: Checking for large files that can be removed...\n"
 sudo find / -type f -size +300M -exec ls -lh {} \;
 
 echo "ADMIN: You can now cleanup the files that you don't want."
