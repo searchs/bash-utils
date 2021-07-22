6f688380b6e6d076213f722d5f29019342f442b2
Ola Ajibode <ola.ajibode@gmail.com>
2021-07-22 13:52:19 +0100

    Feat:  Pass project name as argument

--- create_etl.sh
+++ create_etl.sh
@@ -1,7 +1,14 @@
 
 #!/usr/bin/env bash
+
+###########################################
+
+Usage: generate_etl_project.sh project_name
+
+###########################################
+
 '''
-kfash
+project_name
     -ingestion
         - abfs
         - docs
@@ -19,33 +26,39 @@ kfash
         - run_pipeline.py --config config.yaml --category beauty --run_date YYYYMMDD
 '''
 
+project=$1
+
+echo $project
+
 echo "Creating project folder and starter files" 
 
 echo "Creating ingestion, abfs, docs and docker directories.."
-mkdir -p kfash/ingestion
-touch kfash/ingestion/__init__.py
-mkdir -p kfash/ingestion/abfs
-mkdir -p kfash/ingestion/docs
-mkdir -p kfash/ingestion/docker
+mkdir -p $project/ingestion
+touch $project/ingestion/__init__.py
+mkdir -p $project/ingestion/abfs
+mkdir -p $project/ingestion/docs
+mkdir -p $project/ingestion/docker
 
 echo "Creating config directory.."
-mkdir -p kfash/ingestion/config
-touch kfash/ingestion/config/__init__.py
-touch kfash/ingestion/config/logging_config.yml
-touch kfash/ingestion/config/project.yml
+mkdir -p $project/ingestion/config
+touch $project/ingestion/config/__init__.py
+touch $project/ingestion/config/logging_config.yml
+touch $project/ingestion/config/project.yml
 
 echo "Creating pipeline, test and commons directories"
-mkdir -p kfash/ingestion/pipeline
-touch kfash/ingestion/pipeline/__init__.py
-mkdir -p kfash/ingestion/tests/
-touch kfash/ingestion/tests/__init__.py
-mkdir -p kfash/ingestion/tests/commons
-touch kfash/ingestion/tests/commons/__init__.py
-mkdir -p kfash/ingestion/tests/pipeline
-touch kfash/ingestion/tests/pipeline/__init__.py
-mkdir -p kfash/ingestion/commons
-touch kfash/ingestion/commons/__init__.py
-touch kfash/ingestion/metadata_processing.py
+mkdir -p $project/ingestion/pipeline
+touch $project/ingestion/pipeline/__init__.py
+mkdir -p $project/ingestion/tests/
+touch $project/ingestion/tests/__init__.py
+mkdir -p $project/ingestion/tests/commons
+touch $project/ingestion/tests/commons/__init__.py
+mkdir -p $project/ingestion/tests/pipeline
+touch $project/ingestion/tests/pipeline/__init__.py
+mkdir -p $project/ingestion/commons
+touch $project/ingestion/commons/__init__.py
+touch $project/ingestion/commons/metadata_processing.py
+touch $project/ingestion/commons/constants.py
+touch $project/ingestion/commons/custom_exceptions.py
 touch requirements.txt
 touch README.md
 touch run_pipeline.py
